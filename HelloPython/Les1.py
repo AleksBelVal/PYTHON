403be62def782ec809e06addbd5d6d8f07989ae2
Aleksandr <belyakov71@yandex.ru>
2022-10-11 22:10:17 +0300

    Создана папка READ, gitignore

---
+++ HelloPython/Les1.py
@@ -0,0 +1,15 @@
+# print('Hello, world')
+
+# тип данных и переменная
+# int, float, boolean, str, list, None
+
+a = 2171
+b = 21.71
+# print(a)
+# print(b)
+# print(type(b))
+s = 'world'
+# print(s)  # вывод строки
+# print(a, '-', b, '-', s)
+print(f'{a} - {b} - {s}') # интерполяция (18:35 lec1)
+
